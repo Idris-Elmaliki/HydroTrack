b85269d80a68fcdb7cdcd00d126fe34fa3d1f877
Idris <idris.elmaliki06@gmail.com>
2026-02-11 23:53:34 -0500

    Added the function logic to the UserPreference viewModels

--- Water_Logging_App/app/src/main/java/com/example/water_logging_app/preferenceData/domain/modelData/UserPreferenceData.kt
+++ Water_Logging_App/app/src/main/java/com/example/water_logging_app/preferenceData/domain/modelData/UserPreferenceData.kt
@@ -1,5 +1,10 @@
 package com.example.water_logging_app.preferenceData.domain.modelData
 
+/*
+* All the data is null since I want to include an option where the user doesn't need to give the app any data!
+*
+* Plus this ensures that we don't need to include any default arguments within the viewModels :)
+*/
 data class UserPreferenceData(
     val error : String? = null,
     val name : String? = null,
--- Water_Logging_App/app/src/main/java/com/example/water_logging_app/ui/signUpPage/viewModel/SignUpViewModel.kt
+++ Water_Logging_App/app/src/main/java/com/example/water_logging_app/ui/signUpPage/viewModel/SignUpViewModel.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,15 +16,40 @@ import javax.inject.Inject
 class SignUpViewModel @Inject constructor(
     private val repo : UserPreferenceRepositoryImpl
 ) : ViewModel() {
-    private var _signUpData = MutableStateFlow(UserPreferenceData())
+    private var _signUpData = MutableStateFlow(UserPreferenceData(
+        name = "",
+        dailyGoal = 0,
+        preferredMeasurement = ""
+    ))
     val signUpData : StateFlow<UserPreferenceData> = _signUpData.asStateFlow()
 
-    fun addUserData() {
+
+    fun addUserData(
+        userData : UserPreferenceData
+    ) {
         viewModelScope.launch {
             try {
+                repo.insertUserPreference(
+                    UserPreferenceData(
+                        name = userData.name,
+                        dailyGoal = userData.dailyGoal,
+                        preferredMeasurement = userData.preferredMeasurement
+                    )
+                )
 
+                _signUpData.update { data ->
+                    data.copy(
+                        name = userData.name,
+                        dailyGoal = userData.dailyGoal,
+                        preferredMeasurement = userData.preferredMeasurement
+                    )
+                }
             } catch (e: Exception) {
-
+                _signUpData.update { data ->
+                    data.copy(
+                        error = e.message.toString()
+                    )
+                }
             }
         }
     }
