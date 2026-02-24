bed7697a31c57954cdaa51b799ea3c1498bd15c6
Idris <idris.elmaliki06@gmail.com>
2026-02-23 23:52:34 -0500

    Began working on the UserName Page Ui

---
+++ Water_Logging_App/app/src/main/java/com/example/water_logging_app/ui/signUpPage/screens/_4_UsersNamePageUi.kt
@@ -0,0 +1,14 @@
+package com.example.water_logging_app.ui.signUpPage.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.water_logging_app.ui.signUpPage.viewModel.SignUpViewModel
+
+@Composable
+fun UsersNamePageUi(
+    modifier : Modifier, // just passes in .fillMaxSize()
+    signUpVM : SignUpViewModel,
+    currentNavAction : () -> Unit,
+) {
+
+}
